68d13e2a84a27e9e9938363e17a82c94cb725e55
plachowski <pawel.lachowski@xoog.pl>
2023-12-19 16:56:47 +0100

    PCA

--- 9.analiza_czynnikowa/analiza_czynnikowa.py
+++ 9.analiza_czynnikowa/analiza_czynnikowa.py
@@ -1,26 +1,59 @@
 import pandas as pd
 import numpy as np
-# pip install factor_analyzer
 from factor_analyzer import FactorAnalyzer
+from factor_analyzer.factor_analyzer import calculate_kmo
+# pip install factor_analyzer
+from sklearn.decomposition import PCA
 
+# Wczytanie danych
+dane = pd.read_csv("DS_2000-2009_IND_SAV.tab", sep="\t")
 
-df = pd.read_csv('DS_2000-2009_IND_SAV.tab', sep='\t')
+# Wybór kolumn od ep56_1 do ep56_27
+dane_wybrane = dane.filter(like="ep56_")
 
-# 56.1. Podziwiam ludzi, którzy mają drogie domy, samochody i ubrania
-# 56.27. Bez kar fizycznych nie da się dobrze wychować dzieci.
-df = df[['ep56_1', 'ep56_27']].copy()
+# Zastąpienie wartości spoza zakresu 1-7 przez NaN
+dane_wybrane = dane_wybrane.applymap(lambda x: x if x in range(1, 8) else np.nan)
 
-df.loc[~df['ep56_1'].isin([1, 2, 3, 4, 5, 6, 7]), 'ep56_1'] = np.nan
-df.loc[~df['ep56_27'].isin([1, 2, 3, 4, 5, 6, 7]), 'ep56_27'] = np.nan
+# Usunięcie wierszy, które mają tylko wartości NaN
+dane_czyste = dane_wybrane.dropna()
 
-df = df.dropna()
+# Standaryzacja danych
+dane_standard = (dane_czyste - dane_czyste.mean()) / dane_czyste.std()
 
-fa = FactorAnalyzer(n_factors=3, rotation='varimax')
+# Ustalenie liczby czynników do ekstrakcji
+eigenvalues = np.linalg.eigvals(dane_standard.corr())
+factors_to_extract = np.sum(eigenvalues > 1)
 
-# Dopasuj model analizy czynnikowej do danych
-fa.fit(df)
+# Przeprowadzenie analizy czynnikowej
+fa = FactorAnalyzer(n_factors=factors_to_extract, rotation="varimax")
+fa.fit(dane_standard)
 
-# Wyświetl wyniki analizy czynnikowej
-loadings = pd.DataFrame(fa.loadings_, index=df.columns, columns=[f'Factor{i}' for i in range(1, 3)])
-print("Wyniki analizy czynnikowej:")
-print(loadings)
+# Wyświetlenie wyników analizy czynnikowej
+loadings = pd.DataFrame(fa.loadings_, index=dane_standard.columns, columns=[f'Factor_{i}' for i in range(1, factors_to_extract + 1)])
+print(loadings[abs(loadings) > 0.3].dropna(how='all'))
+
+communality = pd.DataFrame(fa.get_communalities(), index=dane_standard.columns, columns=['Communality'])
+print(communality)
+
+# Przeprowadzenie analizy składowych głównych (PCA)
+pca = PCA()
+pca_results = pca.fit_transform(dane_standard)
+
+# Wyświetlenie wyjaśnionej wariancji przez każdą składową
+print(pca.explained_variance_ratio_)
+
+# Obciążenia składowych głównych nie są bezpośrednio dostępne w sklearn, ale można je obliczyć
+loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+
+# KMO test
+kmo_all, kmo_model = calculate_kmo(dane_standard)
+
+# Jeśli KMO test jest odpowiedni, możemy kontynuować z analizą czynnikową
+if kmo_model < 0.6:
+    raise ValueError("KMO test wynosi mniej niż 0.6, analiza czynnikowa może nie być odpowiednia.")
+
+# Ustalenie liczby czynników do ekstrakcji
+fa = FactorAnalyzer(rotation='varimax')
+fa.fit(dane_standard)
+# Wyświetlenie obciążeń czynnikowych
+print(fa.loadings_)
